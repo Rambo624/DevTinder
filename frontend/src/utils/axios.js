c590612b51d60582504c169570853e584cad1301
Your Name <your_email@example.com>
2024-10-17 12:02:15 +0530

    axios

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -4,8 +4,9 @@ import axios from "axios"
 const baseURL=import.meta.env.VITE_BASE_URL
 const axiosInstance = axios.create({
     baseURL: `${baseURL}`,
-    timeout: 1000,
-    withCredentials:true
+  
+    withCredentials:true,
+   
   });
 
 
